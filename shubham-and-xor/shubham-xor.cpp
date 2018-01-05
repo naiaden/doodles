dd58c218d3c2633c38c3d1ca50133693379d096c
naiaden <louis@naiaden.nl>
2018-01-05 23:40:47 +0100

    Changed the vector into a map with counts

--- shubham-and-xor/shubham-xor.cpp
+++ shubham-and-xor/shubham-xor.cpp
@@ -6,6 +6,7 @@
 #include <limits>
 #include <vector>
 #include <bitset>
+#include <unordered_map>
 
 int main(int argc, char *argv[])
 {
@@ -23,18 +24,31 @@ int main(int argc, char *argv[])
                    std::back_inserter(a),
                    [](const auto& v){ return std::bitset<32>(std::stoul(v)); });
 
-    unsigned results = 0;
-    for(size_t i = 0; i < a.size(); ++i)
+//    std::sort(a.begin(), 
+//              a.end(), 
+//              [](const auto & lhs, const auto & rhs){ return lhs.to_string() < rhs.to_string(); });
+
+    std::unordered_map<std::bitset<32>, unsigned long> m;
+    for(const auto& v: a)
     {
-        for(size_t j = i+1; j < a.size(); ++j)
+        ++m[v];
+    }
+
+    unsigned results = 0;
+    for(auto i = m.begin(); i != m.end(); ++i)
+    {
+        for(auto j = i; j != m.end(); ++j)
         {
-            if(!(a[i] ^ a[j]).count())
+            if(!(i->first ^ j->first).count())
             {
-                ++results;
+                if( i==j)
+                    results += ((i->second-1) * (j->second-1));                
+                else
+                    results += (i->second * j->second);
             }
         }
     }
-    
+      
     std::cout << results << "\n";
     
     return 0;
