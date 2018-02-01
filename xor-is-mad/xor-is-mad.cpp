bdc3bbda319ea53a3f917cda00776136677d7412
naiaden <louis@naiaden.nl>
2018-02-01 11:59:12 +0100

    Rewrote the algorithm

--- xor-is-mad/xor-is-mad.cpp
+++ xor-is-mad/xor-is-mad.cpp
@@ -6,7 +6,24 @@
 #include <limits>
 #include <vector>
 #include <bitset>
+#include <climits>
 #include <unordered_map>
+
+
+std::vector< int > get_bits( unsigned long x ) {
+    std::string chars( std::bitset< sizeof(long) * CHAR_BIT >( x )
+        .to_string( char(0), char(1) ) );
+    return std::vector< int >( chars.begin(), chars.end() );
+}
+
+int getfirst(std::vector<int> bits)
+{
+//    int i = 0;
+    for(int i = 0; i < bits.size(); ++i)
+    {
+        if(bits[i]) return i;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -19,8 +36,14 @@ int main(int argc, char *argv[])
     unsigned long count;
     while(std::getline(std::cin, inputValue))
     {
-       count = 0;
        
+        std::vector<int> vv = get_bits(std::stol(inputValue));
+       count = std::count(vv.begin(), vv.end(), false);
+       std::cout << count << "\t" << getfirst(vv) << "\t" << count-getfirst(vv) << "\n";
+       for(int i : vv)
+        std::cout << i << " ";
+        std::cout << "\n" << count << "\n\n";
+/*       
        unsigned long value = std::stol(inputValue);
        for(int i = 1; i < value; ++i)
        {
@@ -28,7 +51,8 @@ int main(int argc, char *argv[])
             if(i+value == (unsigned long)(i^value)) 
                 ++count;
        }
-       std::cout << count << "\n";
+*/
+
     }
 
     return 0;
